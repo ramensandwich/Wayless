c980f84ffe57093ce2d5cb8ac55dcccd28d7625c
Sean Buckley <sbuckley12489@gmail.com>
2019-07-23 16:06:21 -0700

    Adds GPS to sensor collection. TODO: Cleanup output format. TODO: Make feedback from GPS lock clear

--- Projects/ArduinoTest/ArduinoTest.ino
+++ Projects/ArduinoTest/ArduinoTest.ino
@@ -1,5 +1,20 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
+#include <Adafruit_GPS.h>
+#include <SoftwareSerial.h>
+
+// Connect the GPS Power pin to 5V
+// Connect the GPS Ground pin to ground
+// Connect the GPS TX (transmit) pin to Digital 8
+// Connect the GPS RX (receive) pin to Digital 7
+
+// you can change the pin numbers to match your wiring:
+SoftwareSerial mySerial(8, 7);
+Adafruit_GPS GPS(&mySerial);
+
+// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
+// Set to 'true' if you want to debug and listen to the raw GPS sentences
+#define GPSECHO  true
 
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
@@ -37,9 +52,24 @@ void displayCalStatus(void)
 
 void setup() 
 {
-    Serial.begin(9600);
+    Serial.begin(115200);
 
-    /* Initialise the sensor */
+    // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
+    GPS.begin(9600);
+
+    // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+    GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+
+    // Set the update rate
+    GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+    // For the parsing code to work nicely and have time to sort thru the data, and
+    // print it out we don't suggest using anything higher than 1 Hz
+
+
+    // Request updates on antenna status, comment out to keep quiet
+    GPS.sendCommand(PGCMD_ANTENNA);
+
+    // Initialise the IMU
     if(!bno.begin())
     {
         /* There was a problem detecting the BNO055 ... check your connections */
@@ -52,22 +82,73 @@ void setup()
     bno.setExtCrystalUse(true);
 }
 
+uint32_t timer = millis();
 void loop() 
 {
-    sensors_event_t event; 
-    bno.getEvent(&event);
+    char c = GPS.read();
+    // if you want to debug, this is a good time to do it!
+    if ((c) && (GPSECHO))
+      Serial.write(c);
 
-    //It seems like the X value is relative to magnetic north, but it requires some calibration
-    //  at run time.
-    Serial.print("X: ");
-    Serial.print(event.orientation.x, 4);
-    Serial.print("\tY: ");
-    Serial.print(event.orientation.y, 4);
-    Serial.print("\tZ: ");
-    Serial.print(event.orientation.z, 4);
-    Serial.println("");
+    // if a sentence is received, we can check the checksum, parse it...
+    if (GPS.newNMEAreceived()) {
+        // a tricky thing here is if we print the NMEA sentence, or data
+        // we end up not listening and catching other sentences!
+        // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
+        //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
 
-    displayCalStatus();
+        if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
+          return;  // we can fail to parse a sentence in which case we should just wait for another
+    }
+
+    // if millis() or timer wraps around, we'll just reset it
+    if (timer > millis())  timer = millis();
+
+    // approximately every 2 seconds or so, print out the current stats
+    if (millis() - timer > 2000) {
+    timer = millis(); // reset the timer
+
+        Serial.print("\nTime: ");
+        if (GPS.hour < 10) { Serial.print('0'); }
+        Serial.print(GPS.hour, DEC); Serial.print(':');
+        if (GPS.minute < 10) { Serial.print('0'); }
+        Serial.print(GPS.minute, DEC); Serial.print(':');
+        if (GPS.seconds < 10) { Serial.print('0'); }
+        Serial.print(GPS.seconds, DEC); Serial.print('.');
+        if (GPS.milliseconds < 10) {
+          Serial.print("00");
+        } else if (GPS.milliseconds > 9 && GPS.milliseconds < 100) {
+          Serial.print("0");
+        }
+        Serial.println(GPS.milliseconds);
+        Serial.print("Date: ");
+        Serial.print(GPS.day, DEC); Serial.print('/');
+        Serial.print(GPS.month, DEC); Serial.print("/20");
+        Serial.println(GPS.year, DEC);
+        if (GPS.fix) {
+            Serial.print("Location: ");
+            // latitude in the DDMM.MMMMM format
+            Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+            Serial.print(", ");
+            // longitude in the DDDMM.MMMMM format
+            Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+            Serial.println("");
+        }
+
+            sensors_event_t event; 
+        bno.getEvent(&event);
     
-    delay(100);
+        //It seems like the X value is relative to magnetic north, but it requires some calibration
+        //  at run time.
+        Serial.print("X: ");
+        Serial.print(event.orientation.x, 4);
+        Serial.print("\tY: ");
+        Serial.print(event.orientation.y, 4);
+        Serial.print("\tZ: ");
+        Serial.print(event.orientation.z, 4);
+        Serial.println("");
+    
+        //displayCalStatus();
+    }
+
 }
