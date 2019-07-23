23b3dde1e15d145443b2240cf6f56e09c0804a1c
Sean Buckley <sbuckley12489@gmail.com>
2019-07-22 18:18:45 -0700

    Adding Adafruit libraries for IMU. Adding startercode for Arduino projects

---
+++ Projects/ArduinoTest/ArduinoTest.ino
@@ -0,0 +1,77 @@
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+/**************************************************************************/
+/*
+    Display sensor calibration status
+*/
+/**************************************************************************/
+void displayCalStatus(void)
+{
+  /* Get the four calibration values (0..3) */
+  /* Any sensor data reporting 0 should be ignored, */
+  /* 3 means 'fully calibrated" */
+  uint8_t system, gyro, accel, mag;
+  system = gyro = accel = mag = 0;
+  bno.getCalibration(&system, &gyro, &accel, &mag);
+
+  /* The data should be ignored until the system calibration is > 0 */
+  Serial.print("\t");
+  if (!system)
+  {
+    Serial.print("! ");
+  }
+
+  /* Display the individual values */
+  Serial.print("Sys:");
+  Serial.print(system, DEC);
+  Serial.print(" G:");
+  Serial.print(gyro, DEC);
+  Serial.print(" A:");
+  Serial.print(accel, DEC);
+  Serial.print(" M:");
+  Serial.print(mag, DEC);
+}
+
+void setup() 
+{
+    Serial.begin(9600);
+
+    //TODO: Try setting to compass mode and seeing how accurate it is. All we really need is heading
+
+    /* Initialise the sensor */
+    if(!bno.begin())
+    {
+        /* There was a problem detecting the BNO055 ... check your connections */
+        Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+        while(1);
+    }
+
+    delay(1000);
+
+  bno.setExtCrystalUse(true);
+
+
+}
+
+void loop() 
+{
+    sensors_event_t event; 
+    bno.getEvent(&event);
+
+    //TODO: Verify if the orientation values are absolute (i.e. relative to magnetic north
+    //  and normal to earth's surface) or if they're relative to the starting position
+    Serial.print("X: ");
+    Serial.print(event.orientation.x, 4);
+    Serial.print("\tY: ");
+    Serial.print(event.orientation.y, 4);
+    Serial.print("\tZ: ");
+    Serial.print(event.orientation.z, 4);
+    Serial.println("");
+
+    displayCalStatus();
+    
+    delay(100);
+}
